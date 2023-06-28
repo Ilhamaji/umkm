f1123d0e654b0a219273d93dc27ffa73cf7b8382
ilhamaji <iheadskull@gmail.com>
2023-06-28 07:59:35 +0700

    fourth commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,41 +1,49 @@
 import React from "react";
 import Logo from "../Assets/logo-removebg.png";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-between py-5 w-100 bg-white mx-5 border-b-2 border-l-2 border-r-2 border-slate-200 border-solid">
+    <div className="sticky top-0 flex justify-between py-5 w-100 bg-white mx-5 border-b-2 border-l-2 border-r-2 border-slate-200 border-solid">
       <ul className="ml-5">
         <li className="mr-5">
           <img src={Logo} className="w-20" />
         </li>
       </ul>
       <ul className="m-auto h-100">
-        <li
-          className="inline mx-2 rounded hover:text-white hover:bg-pink-500 py-2 px-4 transition hover:cursor-pointer"
-          onClick={() => navigate("/")}
-        >
-          Home
+        <li className="inline">
+          <NavLink
+            className="inline mx-2 rounded hover:text-white hover:bg-pink-500 py-2 px-4 transition"
+            to="/"
+          >
+            Home
+          </NavLink>
         </li>
-        <li
-          className="inline mx-2 rounded hover:text-white hover:bg-pink-500 py-2 px-4 transition hover:cursor-pointer"
-          onClick={() => navigate("/products")}
-        >
-          Products
+        <li className="inline">
+          <NavLink
+            className="inline mx-2 rounded hover:text-white hover:bg-pink-500 py-2 px-4 transition"
+            to="/products"
+          >
+            Products
+          </NavLink>
         </li>
-        <li
-          className="inline mx-2 rounded hover:text-white hover:bg-pink-500 py-2 px-4 transition hover:cursor-pointer"
-          onClick={() => navigate("/about")}
-        >
-          About
+        <li className="inline">
+          <NavLink
+            className="inline mx-2 rounded hover:text-white hover:bg-pink-500 py-2 px-4 transition"
+            to="/about"
+          >
+            About
+          </NavLink>
         </li>
-        <li
-          className="inline mx-2 rounded hover:text-white hover:bg-pink-500 py-2 px-4 transition hover:cursor-pointer"
-          onClick={() => navigate("/contact")}
-        >
-          Contact
+        <li className="inline">
+          <NavLink
+            className="inline mx-2 rounded hover:text-white hover:bg-pink-500 py-2 px-4 transition"
+            to="/contact"
+          >
+            Contact
+          </NavLink>
         </li>
       </ul>
       <ul className="mr-5">

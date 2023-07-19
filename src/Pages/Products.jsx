7cc73270a8020c94f1dff3087de0410c43b42122
ilhamaji <iheadskull@gmail.com>
2023-07-20 00:03:59 +0700

    eight commit

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,84 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
-import Aksesoris1 from "../Assets/Products/Accessoris/aksesoris (1).jpg";
-import Aksesoris2 from "../Assets/Products/Accessoris/aksesoris (2).jpg";
-import Aksesoris3 from "../Assets/Products/Accessoris/aksesoris (3).jpg";
-import Aksesoris4 from "../Assets/Products/Accessoris/aksesoris (4).jpg";
-import Aksesoris5 from "../Assets/Products/Accessoris/aksesoris (5).jpg";
-import Aksesoris6 from "../Assets/Products/Accessoris/aksesoris (6).jpg";
-import Bouquet1 from "../Assets/Products/Bouquet/bouquet (1).jpg";
-import Bouquet2 from "../Assets/Products/Bouquet/bouquet (2).jpg";
-import Bouquet3 from "../Assets/Products/Bouquet/bouquet (3).jpg";
-import Bouquet4 from "../Assets/Products/Bouquet/bouquet (4).jpg";
-import Dompet1 from "../Assets/Products/Dompet/dompet (1).jpg";
-import Dompet2 from "../Assets/Products/Dompet/dompet (2).jpg";
-import Dompet3 from "../Assets/Products/Dompet/dompet (3).jpg";
-import Dompet4 from "../Assets/Products/Dompet/dompet (4).jpg";
-import Dompet5 from "../Assets/Products/Dompet/dompet (5).jpg";
-import Dompet6 from "../Assets/Products/Dompet/dompet (6).jpg";
-import Dompet7 from "../Assets/Products/Dompet/dompet (7).jpg";
-import Sepatu1 from "../Assets/Products/Sepatu/sepatu (1).jpg";
-import Sepatu2 from "../Assets/Products/Sepatu/sepatu (2).jpg";
-import Sepatu3 from "../Assets/Products/Sepatu/sepatu (3).jpg";
-import Sepatu4 from "../Assets/Products/Sepatu/sepatu (4).jpg";
-import Sepatu5 from "../Assets/Products/Sepatu/sepatu (5).jpg";
-import Tas1 from "../Assets/Products/Tas/tas (1).jpg";
-import Tas2 from "../Assets/Products/Tas/tas (2).jpg";
-import Tas3 from "../Assets/Products/Tas/tas (3).jpg";
-import Tas4 from "../Assets/Products/Tas/tas (4).jpg";
-import Tas5 from "../Assets/Products/Tas/tas (5).jpg";
-import Tas6 from "../Assets/Products/Tas/tas (6).jpg";
-import Tas7 from "../Assets/Products/Tas/tas (7).jpg";
-import Tas8 from "../Assets/Products/Tas/tas (8).jpg";
-import Topi1 from "../Assets/Products/Topi/topi (1).jpg";
-import Topi2 from "../Assets/Products/Topi/topi (2).jpg";
-import Topi3 from "../Assets/Products/Topi/topi (3).jpg";
-import Topi4 from "../Assets/Products/Topi/topi (4).jpg";
-import Topi5 from "../Assets/Products/Topi/topi (5).jpg";
-import Topi6 from "../Assets/Products/Topi/topi (6).jpg";
 import Blob from "../Assets/blob-scene-haikei.svg";
 import LazyLoad from "react-lazy-load";
+import axios from "axios";
 
 export default function Products() {
-  const [items, setItems] = useState([
-    { nama: "Aksesoris", gambar: Aksesoris1 },
-    { nama: "Aksesoris", gambar: Aksesoris2 },
-    { nama: "Aksesoris", gambar: Aksesoris3 },
-    { nama: "Aksesoris", gambar: Aksesoris4 },
-    { nama: "Aksesoris", gambar: Aksesoris5 },
-    { nama: "Aksesoris", gambar: Aksesoris6 },
-    { nama: "Bouquet", gambar: Bouquet1 },
-    { nama: "Bouquet", gambar: Bouquet2 },
-    { nama: "Bouquet", gambar: Bouquet3 },
-    { nama: "Bouquet", gambar: Bouquet4 },
-    { nama: "Dompet", gambar: Dompet1 },
-    { nama: "Dompet", gambar: Dompet2 },
-    { nama: "Dompet", gambar: Dompet3 },
-    { nama: "Dompet", gambar: Dompet4 },
-    { nama: "Dompet", gambar: Dompet5 },
-    { nama: "Dompet", gambar: Dompet6 },
-    { nama: "Dompet", gambar: Dompet7 },
-    { nama: "Sepatu", gambar: Sepatu1 },
-    { nama: "Sepatu", gambar: Sepatu2 },
-    { nama: "Sepatu", gambar: Sepatu3 },
-    { nama: "Sepatu", gambar: Sepatu4 },
-    { nama: "Sepatu", gambar: Sepatu5 },
-    { nama: "Tas", gambar: Tas1 },
-    { nama: "Tas", gambar: Tas2 },
-    { nama: "Tas", gambar: Tas3 },
-    { nama: "Tas", gambar: Tas4 },
-    { nama: "Tas", gambar: Tas5 },
-    { nama: "Tas", gambar: Tas6 },
-    { nama: "Tas", gambar: Tas7 },
-    { nama: "Tas", gambar: Tas8 },
-    { nama: "Topi", gambar: Topi1 },
-    { nama: "Topi", gambar: Topi2 },
-    { nama: "Topi", gambar: Topi3 },
-    { nama: "Topi", gambar: Topi4 },
-    { nama: "Topi", gambar: Topi5 },
-    { nama: "Topi", gambar: Topi6 },
-  ]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const loadImage = async () => {
+      const response = await axios.get(
+        process.env.PUBLIC_URL + "/API/Products.json"
+      );
+
+      setProducts(response.data);
+    };
+    loadImage();
+  }, []);
 
   return (
     <>
@@ -86,23 +25,22 @@ export default function Products() {
 
       <h1>Aksesoris</h1>
       <div className="mx-5 flex flex-wrap justify-center">
-        {items.map((item, i) => {
-          if (item.nama === "Aksesoris") {
+        {products.map(({ id, name, slug, image }) => {
+          if (name === "Aksesoris") {
             return (
               <div
-                key={i}
+                key={id}
                 className="hover:shadow-none duration-300 bg-neutral-800 cursor-default py-7 h-fit w-96 rounded-3xl my-5 mx-5 shadow-lg shadow-pink-500 group"
               >
                 <center>
                   <LazyLoad
                     effect="blur"
-                    placeholder={Blob}
                     className="h-48 overflow-hidden w-85 duration-300 rounded-3xl"
                   >
                     <img
-                      src={item.gambar}
+                      src={process.env.PUBLIC_URL + `${image}`}
                       className="flex z-0 h-100 -translate-y-10 md:-translate-y-36 group-hover:scale-150 duration-300"
-                      alt={i}
+                      alt={id}
                       placeholder={Blob}
                     />
                   </LazyLoad>
@@ -110,7 +48,7 @@ export default function Products() {
                   <div className="w-85 h-px my-5 bg-slate-300"></div>
                   <div className="flex justify-between px-7">
                     <div className="text-lg sm:text-4xl text-white w-10 font-semibold">
-                      {item.nama}
+                      {name}
                     </div>
                     <div className="py-2 w-30 sm:w-40 text-xs px-5 sm:px-0 sm:text-base shadow-md shadow-pink-500 hover:shadow-none hover:bg-neutral-300 font-semibold rounded-3xl bg-white cursor-pointer duration-200">
                       🛒 Beli Sekarang
@@ -127,23 +65,22 @@ export default function Products() {
 
       <h1>Bouquet</h1>
       <div className="mx-5 flex flex-wrap justify-center">
-        {items.map((item, i) => {
-          if (item.nama === "Bouquet") {
+        {products.map(({ id, name, slug, image }) => {
+          if (name === "Bouquet") {
             return (
               <div
-                key={i}
+                key={id}
                 className="hover:shadow-none duration-300 bg-neutral-800 cursor-default py-7 h-fit w-96 rounded-3xl my-5 mx-5 shadow-lg shadow-pink-500 group"
               >
                 <center>
                   <LazyLoad
                     effect="blur"
-                    placeholder={Blob}
                     className="h-48 overflow-hidden w-85 duration-300 rounded-3xl"
                   >
                     <img
-                      src={item.gambar}
+                      src={process.env.PUBLIC_URL + `${image}`}
                       className="flex z-0 h-100 -translate-y-10 md:-translate-y-36 group-hover:scale-150 duration-300"
-                      alt={i}
+                      alt={id}
                       placeholder={Blob}
                     />
                   </LazyLoad>
@@ -151,7 +88,7 @@ export default function Products() {
                   <div className="w-85 h-px my-5 bg-slate-300"></div>
                   <div className="flex justify-between px-7">
                     <div className="text-lg sm:text-4xl text-white w-10 font-semibold">
-                      {item.nama}
+                      {name}
                     </div>
                     <div className="py-2 w-30 sm:w-40 text-xs px-5 sm:px-0 sm:text-base shadow-md shadow-pink-500 hover:shadow-none hover:bg-neutral-300 font-semibold rounded-3xl bg-white cursor-pointer duration-200">
                       🛒 Beli Sekarang
@@ -168,23 +105,22 @@ export default function Products() {
 
       <h1>Dompet</h1>
       <div className="mx-5 flex flex-wrap justify-center">
-        {items.map((item, i) => {
-          if (item.nama === "Dompet") {
+        {products.map(({ id, name, slug, image }) => {
+          if (name === "Dompet") {
             return (
               <div
-                key={i}
+                key={id}
                 className="hover:shadow-none duration-300 bg-neutral-800 cursor-default py-7 h-fit w-96 rounded-3xl my-5 mx-5 shadow-lg shadow-pink-500 group"
               >
                 <center>
                   <LazyLoad
                     effect="blur"
-                    placeholder={Blob}
                     className="h-48 overflow-hidden w-85 duration-300 rounded-3xl"
                   >
                     <img
-                      src={item.gambar}
+                      src={process.env.PUBLIC_URL + `${image}`}
                       className="flex z-0 h-100 -translate-y-10 md:-translate-y-36 group-hover:scale-150 duration-300"
-                      alt={i}
+                      alt={id}
                       placeholder={Blob}
                     />
                   </LazyLoad>
@@ -192,7 +128,7 @@ export default function Products() {
                   <div className="w-85 h-px my-5 bg-slate-300"></div>
                   <div className="flex justify-between px-7">
                     <div className="text-lg sm:text-4xl text-white w-10 font-semibold">
-                      {item.nama}
+                      {name}
                     </div>
                     <div className="py-2 w-30 sm:w-40 text-xs px-5 sm:px-0 sm:text-base shadow-md shadow-pink-500 hover:shadow-none hover:bg-neutral-300 font-semibold rounded-3xl bg-white cursor-pointer duration-200">
                       🛒 Beli Sekarang
@@ -209,23 +145,22 @@ export default function Products() {
 
       <h1>Sepatu</h1>
       <div className="mx-5 flex flex-wrap justify-center">
-        {items.map((item, i) => {
-          if (item.nama === "Sepatu") {
+        {products.map(({ id, name, slug, image }) => {
+          if (name === "Sepatu") {
             return (
               <div
-                key={i}
+                key={id}
                 className="hover:shadow-none duration-300 bg-neutral-800 cursor-default py-7 h-fit w-96 rounded-3xl my-5 mx-5 shadow-lg shadow-pink-500 group"
               >
                 <center>
                   <LazyLoad
                     effect="blur"
-                    placeholder={Blob}
                     className="h-48 overflow-hidden w-85 duration-300 rounded-3xl"
                   >
                     <img
-                      src={item.gambar}
+                      src={process.env.PUBLIC_URL + `${image}`}
                       className="flex z-0 h-100 -translate-y-10 md:-translate-y-36 group-hover:scale-150 duration-300"
-                      alt={i}
+                      alt={id}
                       placeholder={Blob}
                     />
                   </LazyLoad>
@@ -233,7 +168,7 @@ export default function Products() {
                   <div className="w-85 h-px my-5 bg-slate-300"></div>
                   <div className="flex justify-between px-7">
                     <div className="text-lg sm:text-4xl text-white w-10 font-semibold">
-                      {item.nama}
+                      {name}
                     </div>
                     <div className="py-2 w-30 sm:w-40 text-xs px-5 sm:px-0 sm:text-base shadow-md shadow-pink-500 hover:shadow-none hover:bg-neutral-300 font-semibold rounded-3xl bg-white cursor-pointer duration-200">
                       🛒 Beli Sekarang
@@ -250,23 +185,22 @@ export default function Products() {
 
       <h1>Tas</h1>
       <div className="mx-5 flex flex-wrap justify-center">
-        {items.map((item, i) => {
-          if (item.nama === "Tas") {
+        {products.map(({ id, name, slug, image }) => {
+          if (name === "Tas") {
             return (
               <div
-                key={i}
+                key={id}
                 className="hover:shadow-none duration-300 bg-neutral-800 cursor-default py-7 h-fit w-96 rounded-3xl my-5 mx-5 shadow-lg shadow-pink-500 group"
               >
                 <center>
                   <LazyLoad
                     effect="blur"
-                    placeholder={Blob}
                     className="h-48 overflow-hidden w-85 duration-300 rounded-3xl"
                   >
                     <img
-                      src={item.gambar}
+                      src={process.env.PUBLIC_URL + `${image}`}
                       className="flex z-0 h-100 -translate-y-10 md:-translate-y-36 group-hover:scale-150 duration-300"
-                      alt={i}
+                      alt={id}
                       placeholder={Blob}
                     />
                   </LazyLoad>
@@ -274,7 +208,7 @@ export default function Products() {
                   <div className="w-85 h-px my-5 bg-slate-300"></div>
                   <div className="flex justify-between px-7">
                     <div className="text-lg sm:text-4xl text-white w-10 font-semibold">
-                      {item.nama}
+                      {name}
                     </div>
                     <div className="py-2 w-30 sm:w-40 text-xs px-5 sm:px-0 sm:text-base shadow-md shadow-pink-500 hover:shadow-none hover:bg-neutral-300 font-semibold rounded-3xl bg-white cursor-pointer duration-200">
                       🛒 Beli Sekarang
@@ -291,23 +225,22 @@ export default function Products() {
 
       <h1>Topi</h1>
       <div className="mx-5 flex flex-wrap justify-center">
-        {items.map((item, i) => {
-          if (item.nama === "Topi") {
+        {products.map(({ id, name, slug, image }) => {
+          if (name === "Topi") {
             return (
               <div
-                key={i}
+                key={id}
                 className="hover:shadow-none duration-300 bg-neutral-800 cursor-default py-7 h-fit w-96 rounded-3xl my-5 mx-5 shadow-lg shadow-pink-500 group"
               >
                 <center>
                   <LazyLoad
                     effect="blur"
-                    placeholder={Blob}
                     className="h-48 overflow-hidden w-85 duration-300 rounded-3xl"
                   >
                     <img
-                      src={item.gambar}
+                      src={process.env.PUBLIC_URL + `${image}`}
                       className="flex z-0 h-100 -translate-y-10 md:-translate-y-36 group-hover:scale-150 duration-300"
-                      alt={i}
+                      alt={id}
                       placeholder={Blob}
                     />
                   </LazyLoad>
@@ -315,7 +248,7 @@ export default function Products() {
                   <div className="w-85 h-px my-5 bg-slate-300"></div>
                   <div className="flex justify-between px-7">
                     <div className="text-lg sm:text-4xl text-white w-10 font-semibold">
-                      {item.nama}
+                      {name}
                     </div>
                     <div className="py-2 w-30 sm:w-40 text-xs px-5 sm:px-0 sm:text-base shadow-md shadow-pink-500 hover:shadow-none hover:bg-neutral-300 font-semibold rounded-3xl bg-white cursor-pointer duration-200">
                       🛒 Beli Sekarang
--- src/Pages/Products/Bouquet.jsx
+++ src/Pages/Products/Bouquet.jsx
@@ -1,5 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
+import LazyLoad from "react-lazy-load";
+import axios from "axios";
 
 export default function Bouquet() {
-  return <div>Bouquet</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadImage = async () => {
+      const response = await axios.get(
+        process.env.PUBLIC_URL + "/API/Products.json"
+      );
+
+      setProducts(response.data);
+      setLoading(false);
+    };
+    loadImage();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      {loading ? (
+        <div className="flex justify-center">
+          <div className="animate-spin text-4xl bold text-white">↻</div>
+        </div>
+      ) : (
+        <div className="mx-5 flex flex-wrap justify-center">
+          {products.map(({ id, name, slug, image }) => {
+            if (name === "Bouquet") {
+              return (
+                <div
+                  key={id}
+                  className="hover:shadow-none duration-300 bg-neutral-800 cursor-default py-7 h-fit w-96 rounded-3xl my-5 mx-5 shadow-lg shadow-pink-500 group"
+                >
+                  <center>
+                    <LazyLoad
+                      effect="blur"
+                      className="h-48 overflow-hidden w-85 duration-300 rounded-3xl"
+                    >
+                      <img
+                        src={process.env.PUBLIC_URL + `${image}`}
+                        className="flex z-0 h-100 -translate-y-10 md:-translate-y-36 group-hover:scale-150 duration-300"
+                        alt={id}
+                      />
+                    </LazyLoad>
+
+                    <div className="w-85 h-px my-5 bg-slate-300"></div>
+                    <div className="flex justify-between px-7">
+                      <div className="text-lg sm:text-4xl text-white w-10 font-semibold">
+                        {name}
+                      </div>
+                      <div className="py-2 w-30 sm:w-40 text-xs px-5 sm:px-0 sm:text-base shadow-md shadow-pink-500 hover:shadow-none hover:bg-neutral-300 font-semibold rounded-3xl bg-white cursor-pointer duration-200">
+                        🛒 Beli Sekarang
+                      </div>
+                    </div>
+                  </center>
+                </div>
+              );
+            }
+          })}
+        </div>
+      )}
+
+      <Footer />
+    </>
+  );
 }
